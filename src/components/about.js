5e6d3f47c324fe39c5c328ade0e9bf094780c6ab
Dmitriy Ivanov <dmitriy.p86@ya.ru>
2020-06-23 16:17:29 +0600

    changed-about

--- src/components/about.js
+++ src/components/about.js
@@ -11,10 +11,30 @@ const About = () => (
                   <div className="content has-text-centered">
                      <h2 className="title is-size-3 is-spaced">About</h2>
                      <p className="subtitle is-size-5 column is-three-fifths is-offset-one-fifth">
-                        Hello. I have 3+ years of software development. 
+                        
+                        Hello, I have been developing software for more than 4 years.
+                        And now I'm looking for a job as an experienced junior Javascript/React developer. 
+                        Junior because there is no significant experience in web development. 
+                        And experienced because I practiced a lot in Javascript and React, part of my code can be viewed on GitHub.
+                        I practiced not only React, but everything that can work with it: Node, Next, Electron, 
+                        various serverless cloud solutions and APIs (AWS, Firebase, SendGrid).
+                        Plus I previously worked in development on 1C:Enterprise (based on Visual Basic and SQL). 
+
+
+                        {/* Hello, I have been developing software for more than 4 years.
+                        And now I'm looking for a job as an experienced junior Javascript/React developer. 
+                        Junior because there is no significant experience in web development. And experienced for two reasons.
+                        First, I practiced a lot in Javascript and React, part of my code can be viewed on github.
+                        I practiced not only React, but everything that can work with it: Node, Next, Electron, 
+                        various serverless cloud solutions and connecting a ready-made API (AWS, Firebase, SendGrid).
+                        And the second reason - I previously worked in development on 1C:Enterprise (it's a mixture of Visual Basic and SQL). 
+                        In addition, I sometimes practice in solving various exercises, such as in Codewars. */}
+
+                        {/* Hello. I have 3+ years of software development. 
                         In recent years, I have dived into web development.
                         I know well: React and its surrounding technologies, pure JavaScript with ES6 syntax. 
-                        I also have some knowledge related to Node, Express, Meteor, Next, GraphQL, Gatsby and others.</p>
+                        I also have some knowledge related to Node, Express, Meteor, Next, GraphQL, Gatsby and others. */}
+                     </p>
                   </div>
                   <div className="column is-half is-offset-one-quarter">
                      <div className='tags tech is-clearfix has-text-centered center'>
@@ -29,6 +49,10 @@ const About = () => (
                         <span className="tag bootstarp">Bootstrap</span>
                         <span className="tag is-primary">Bulma</span>
                         <span className="tag is-warning">Firebase</span>
+                        <span className="tag jest">Jest</span>
+                        <span className="tag express">Express</span>
+                        <span className="tag mongodb">Mongo</span>
+                        
                      </div>
                   </div>
                </div>
