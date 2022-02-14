c58c73034662adfc444801c00c59ecbd028ffb5f
Ivanov Dmitriy <dmitriy.p86@ya.ru>
2022-02-14 17:10:21 +0600

    layout_has_been_moved_to_index_page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
-import Layout from '../components/layout';
-import '../style.scss'
+import '../style.scss';
+import Helmet from '../components/helmet';
+import Header from '../components/header';
+import About from '../components/about';
+import Portfolio from '../components/portfolio/portfolio-section';
+import Contact from '../components/contact';
+import Footer from '../components/footer';
 
-const IndexPage = () => <Layout />;
+const IndexPage = () => {
+  return (
+    <>      
+      <Helmet />
+      <Header />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  )
+};
 
 export default IndexPage;
